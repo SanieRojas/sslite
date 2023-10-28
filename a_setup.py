4f974ea22ed09c4a2d6c4bc33f30be7baa74d4ea
Rojas Lobo <sanie.s.rojas.lobo@accenture.com>
2023-10-28 12:02:08 -0300

    Submitting main files of the program

---
+++ a_setup.py
@@ -0,0 +1,24 @@
+topics =  ["Middle-East-Tensions", 
+               "Rusia-China-Alliance", 
+               "US-Military", 
+               "Cybersecurity-threats", 
+               "energy-resources", 
+               "aerospace", 
+               "infrastructure-vulnerabilities", 
+               "financial-markets", 
+               "supranational-events"]
+
+FOLDER_RAW_GCS = "gs://subject-screener1/raw_data/"
+FOLDER_DFS_GCS = "gs://subject-screener1/dfs/"
+FOLDER_TXTS_GCS = "gs://subject-screener1/txts/"
+FOLDER_JSON_GCS = "gs://subject-screener1/jsons/"
+BUCKET_NAME = "subject-screener1"
+DATASET_ID = "compiled_data"
+TABLE_ID_1 = "news_by_subject"
+TABLE_ID_2 = "hourly_scores_by_subject"
+
+folder_processed = 'files/processed'
+folder_raw = "files/raw"
+
+
+
