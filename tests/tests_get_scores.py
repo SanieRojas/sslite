989e2c9926e60a9a5895691997dece6a62fa7583
Sanie Rojas <mssartemis@gmail.com>
2023-11-16 14:10:49 -0300

    Testing changes

--- tests/tests_get_scores.py
+++ tests/tests_get_scores.py
@@ -1,30 +1,7 @@
 '''testing the dataframe processing functions'''
 import unittest
 import pandas as pd
-from dags import get_tokens, get_scores  # Replace
-
-
-class TestGetTokens(unittest.TestCase):
-    ''' tests Get_Tokens function '''
-    def test_empty_input(self):
-        ''' tests result continuance when input is empty '''
-        result = get_tokens('')
-        self.assertEqual(result, [])
-
-    def test_single_word_input(self):
-        ''' tests lower casing'''
-        result = get_tokens('Hello')
-        self.assertEqual(result, ['hello'])
-
-    def test_punctuation_removal(self):
-        ''' tests split and punctuation removal'''
-        result = get_tokens('Hello, world!')
-        self.assertEqual(result, ['hello', 'world'])
-
-    def test_stopwords_removal(self):
-        ''' tests removal of stopwords'''
-        result = get_tokens('The quick brown fox')
-        self.assertEqual(result, ['quick', 'brown', 'fox'])
+from functions import get_scores  
 
 
 class TestGetScores(unittest.TestCase):
